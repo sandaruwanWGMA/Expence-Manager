2d24763021705aa81682fb49ab3cc52581f256d9
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-09-13 20:37:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
+import ExpenceDetails from "./components/ExpenceDetails";
+
+function App() {
+  return (
+    <>
+      <ExpenceDetails></ExpenceDetails>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenceDetails.tsx
@@ -0,0 +1,13 @@
+function ExpenceDetails() {
+  return (
+    <form>
+      <h5>Description</h5>
+      <input className="form-control" type="text" placeholder="" />
+      <h5>Amount</h5>
+      <input className="form-control" type="text" placeholder="" />
+      <h5>Category</h5>
+    </form>
+  );
+}
+
+export default ExpenceDetails;
