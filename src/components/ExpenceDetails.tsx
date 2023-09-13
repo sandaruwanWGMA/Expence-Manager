f31147c793188e9663d121493093f414b589bfdb
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-09-13 21:42:21 +0530

    commit

--- src/components/ExpenceDetails.tsx
+++ src/components/ExpenceDetails.tsx
@@ -1,3 +1,5 @@
+import "../styles/ExpenceDetails.css";
+
 function ExpenceDetails() {
   return (
     <form>
@@ -6,6 +8,18 @@ function ExpenceDetails() {
       <h5>Amount</h5>
       <input className="form-control" type="text" placeholder="" />
       <h5>Category</h5>
+
+      <div className="form-group">
+        <select className="custom-select" required>
+          <option value="">Open this select menu</option>
+          <option>Glosaries</option>
+          <option>Utilities</option>
+          <option>Entertainment</option>
+        </select>
+        <div className="invalid-feedback">
+          Example invalid custom select feedback
+        </div>
+      </div>
     </form>
   );
 }
